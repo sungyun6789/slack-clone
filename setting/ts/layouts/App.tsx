efcd3cb81baadabfc06b678af44c08a427933466
sungyun <sungyun5423@gmail.com>
2021-06-06 15:49:42 +0900

    [Setting] directory-structure & react-router

--- setting/ts/layouts/App.tsx
+++ setting/ts/layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LogIn from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
-const App = () => {
-  return <div>초기 세팅입니다!</div>;
+const App: React.FC = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route exact path="/login" component={LogIn} />
+      <Route exact path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
